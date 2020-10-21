2c45723c1342cadcea781e2e349231de0f9330ba
helpower2 <jeffreymonnink@gmail.com>
2020-10-21 20:38:36 +0200

    clients can connect and move.

---
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : Singleton<NetworkManager>
+{
+    public GameObject playerPrefab;
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 50;
+
+        Server.Start(50, 26950);
+    }
+
+    private void OnApplicationQuit()
+    {
+        Server.Stop();
+    }
+
+    public Player InstantiatePlayer()
+    {
+        return Instantiate(playerPrefab, new Vector3(0f, 0.5f, 0f), Quaternion.identity).GetComponent<Player>();
+    }
+}
